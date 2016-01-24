f185a726db8948ae31167410da119e6735bd93e5
Mikhail Delport <mikhail@delport.ca>
2016-01-23 20:54:34 -0500

    Added users endpoint, read only for now

--- app/views.py
+++ app/views.py
@@ -1,19 +1,11 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from app.serializers import UserSerializer, GroupSerializer
+from app.serializers import UserSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    This viewset automatically provides `list` and `detail` actions.
     """
-    queryset = User.objects.all().order_by('-date_joined')
+    queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
--- plexrequests/urls.py
+++ plexrequests/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from movies import views
+from rest_framework.routers import DefaultRouter
+from movies import views as movie_views
+from app import views as app_views
 
+router = DefaultRouter()
+router.register(r'users', app_views.UserViewSet)
 
 urlpatterns = [
     url(r'^api/auth/', include('rest_framework.urls')),
-    url(r'^api/movies/$', views.MovieList.as_view()),
-    url(r'^api/movies/(?P<pk>[0-9]+)/$', views.MovieDetail.as_view()),
+    url(r'^api/movies/$', movie_views.MovieList.as_view()),
+    url(r'^api/movies/(?P<pk>[0-9]+)/$', movie_views.MovieDetail.as_view()),
+    url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
 ]
