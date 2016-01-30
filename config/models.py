a3c3fec5a297c06c06c28407b319ced0c205d84f
Mikhail Delport <mikhail@delport.ca>
2016-01-30 14:31:54 -0500

    Default settings added

--- config/models.py
+++ config/models.py
@@ -19,6 +19,37 @@ class Config(models.Model):
     auth_required = models.BooleanField(default=False)
     auth_plextoken = models.CharField(max_length=255, default='abcd1234')
 
+    couchpotato_enabled = models.BooleanField(default=False)
+    couchpotato_host = models.CharField(max_length=255, default='192.168.0.1')
+    couchpotato_port = models.IntegerField(default=5050)
+    couchpotato_api = models.CharField(max_length=255, default='abcd1234')
+    couchpotato_ssl = models.BooleanField(default=False)
+    couchpotato_directory = models.CharField(max_length=255, default='')
+
+    sickrage_enabled = models.BooleanField(default=False)
+    sickrage_host = models.CharField(max_length=255, default='192.168.0.1')
+    sickage_port = models.IntegerField(default=8081)
+    sickrage_api = models.CharField(max_length=255, default='abcd1234')
+    sickrage_ssl = models.BooleanField(default=False)
+    sickrage_directory = models.CharField(max_length=255, default='')
+
+    sonarr_enabled = models.BooleanField(default=False)
+    sonarr_host = models.CharField(max_length=255, default='192.168.0.1')
+    sonarr_port = models.IntegerField(default=8989)
+    sonarr_api = models.CharField(max_length=255, default='abcd1234')
+    sonarr_ssl = models.BooleanField(default=False)
+    sonarr_directory = models.CharField(max_length=255, default='')
+    sonarr_quality = models.IntegerField(default=1)
+    sonarr_root = models.CharField(max_length=255, default='/path/to/root/tv/folder')
+    sonarr_season_folders = models.BooleanField(default=True)
+
+    pushbullet_enabled = models.BooleanField(default=False)
+    pushbullet_api = models.CharField(max_length=255, default='abcd1234')
+
+    pushover_enabled = models.BooleanField(default=False)
+    pushover_api = models.CharField(max_length=255, default='abcd1234')
+    pushover_user = models.CharField(max_length=255, default='abcd1234')
+
     singleton_enforce = models.IntegerField(default=1, unique=True)
 
     def save(self, *args, **kwargs):
