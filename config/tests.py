30b9c5a29b2d50c82b89a1a5bf3e767e634b2332
loken <mikhail@delport.ca>
2016-01-31 20:54:48 -0500

    Tests for Plex Authentication

--- config/tests.py
+++ config/tests.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 from .models import Config
 from django.contrib.auth.models import User
+import httpretty
 
 
 class ConfigEndPointTests(APITestCase):
@@ -25,7 +26,59 @@ class ConfigEndPointTests(APITestCase):
         Config.objects.create()
         client = APIClient()
         client.force_authenticate(user=user)
-        data = { 'site_title': 'New Title' }
+        data = {'site_title': 'New Title'}
         response = client.patch('/api/settings/1/', data)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    @httpretty.activate
+    def test_new_plex_login(self):
+        httpretty.register_uri(httpretty.POST, "https://plex.tv/users/sign_in.json",
+                               body='{"user":{"email": "test@email.com"}}',
+                               status=201)
+
+        httpretty.register_uri(httpretty.GET, "https://plex.tv/pms/friends/all?X-Plex-Token=abcd1234",
+                               body="""<?xml version='1.0' encoding='UTF-8'?><MediaContainer>
+                                    <User username='user' email='test@email.com'><Server/></User></MediaContainer>""",
+                               status=200)
+
+        Config.objects.create()
+        client = APIClient()
+        data = {'username': 'user', 'password': 'pass'}
+        response = client.post('/api/login/', data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.get().username, 'user')
+
+    @httpretty.activate
+    def test_invalid_plex_login(self):
+        httpretty.register_uri(httpretty.POST, "https://plex.tv/users/sign_in.json",
+                               body='Denied Access',
+                               status=401)
+
+        Config.objects.create()
+        client = APIClient()
+        data = {'username': 'user', 'password': 'pass'}
+        response = client.post('/api/login/', data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 0)
+
+    @httpretty.activate
+    def test_invalid_plex_friend(self):
+        httpretty.register_uri(httpretty.POST, "https://plex.tv/users/sign_in.json",
+                               body='{"user":{"email": "test@email.com"}}',
+                               status=201)
+
+        httpretty.register_uri(httpretty.GET, "https://plex.tv/pms/friends/all?X-Plex-Token=abcd1234",
+                               body="""<?xml version='1.0' encoding='UTF-8'?><MediaContainer>
+                                    <User username='another' email='test@email.com'><Server/></User></MediaContainer>""",
+                               status=200)
+
+        Config.objects.create()
+        client = APIClient()
+        data = {'username': 'user', 'password': 'pass'}
+        response = client.post('/api/login/', data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 0)
