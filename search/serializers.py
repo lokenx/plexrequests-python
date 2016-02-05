fa9483e020f264d63c24665b3f2a1e42b07593d5
Mikhail Delport <mikhail@delport.ca>
2016-02-05 10:56:10 -0500

    Changed DateField to CharField as P2.7 had issues with TMDB package results

--- search/serializers.py
+++ search/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 class MovieResultsSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
-    release_date = serializers.DateField()
+    release_date = serializers.CharField(max_length=255)
     overview = serializers.CharField(max_length=1000)
     poster_path = serializers.CharField(max_length=255)
     id = serializers.CharField(max_length=255)
